e0e5177573b89fdbf7ebacc6074a7e1c5e1047bb
Reema Thakur <reemathakur2114@gmail.com>
2024-06-14 17:52:50 +0530

    edit & delete post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const path = require("path");
 const { validationResult } = require("express-validator");
 const Post = require("../models/posts");
 
@@ -26,7 +28,6 @@ exports.createPost = (req, res, next) => {
     throw error; // it will now try to reach next error handling function in application : sync code
   }
   if (!req.file) {
-    console.log("here");
     const error = new Error("No image");
     error.statusCode = 422;
     throw error;
@@ -67,6 +68,81 @@ exports.getPost = (req, res, next) => {
         throw error; // inside async, so it will get caught in catch block
       }
       res.status(200).json({ message: "Post fetched.", post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err); // async code
+    });
+};
+
+exports.updatePost = (req, res, next) => {
+  const postId = req.params.postId;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation failed");
+    error.statusCode = 422;
+    throw error; // it will now try to reach next error handling function in application : sync code
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  let imageUrl = req.body.image;
+  if (req.file) {
+    imageUrl = req.file.path;
+  }
+  if (!imageUrl) {
+    const error = new Error("No image");
+    error.statusCode = 422;
+    throw error;
+  }
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404; // not found
+        throw error; // inside async, so it will get caught in catch block
+      }
+      if (imageUrl !== post.imageUrl) {
+        clearImage(post.imageUrl);
+      }
+      post.title = title;
+      post.imageUrl = imageUrl;
+      post.content = content;
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({ message: "Post Updated", post: result });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err); // async code
+    });
+};
+
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
+
+exports.deletePost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("Could not find post");
+        error.statusCode = 404; // not found
+        throw error; // inside async, so it will get caught in catch block
+      }
+      // Check user authentication
+      clearImage(post.imageUrl);
+      return Post.findByIdAndDelete(postId);
+    })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({ message: "Deleted post" });
     })
     .catch((err) => {
       if (!err.statusCode) {
